80ec4a961af4579196c6bfac29e59bc72b3d3777
Disolutionsmx <daniel.enco12@gmail.com>
2018-07-11 02:29:28 -0500

    Assistant
    aun faltan las rutas, espero a que termines para agregarlas

---
+++ client/src/app/components/asistencia-add.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+import { ClienteService } from '../services/cliente.service';
+import { GLOBAL } from '../services/global';
+import { Assistant } from '../models/assistant';
+
+@Component({
+    selector: 'assistant-add',
+    templateUrl: '../views/assistant-add.html',
+    providers: [ClienteService]
+})
+export class AssistantaddComponent implements OnInit {
+    public titulo: String;
+    public assistant: Assistant;
+    public identity;
+    public token;
+    public url: string;
+    public alertMessage;
+
+    constructor(
+        private _route: ActivatedRoute,
+        private _router: Router,
+        private _clienteService: ClienteService,
+    ) {
+        this.titulo = 'Crear Asistencia';
+        this.url = GLOBAL.url;
+        this.assistant = new Assistant('','');
+
+        this.identity = this._clienteService.getidentity();
+        this.token = this._clienteService.getToken();
+
+
+    }
+   
+    ngOnInit() {
+        console.log('Assistantadd.component.cargado');
+    }
+
+    onSumit(){
+        console.log(this.assistant);
+
+        this._route.params.forEach((params:Params) =>{
+            let turno_id =params['turno'];
+            this.assistant.turno = turno_id;
+        }
+    )
+    }
+}
