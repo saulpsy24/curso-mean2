a8036dcc0b4dad82384ff473922055f14ccdb3ab
saulpsy24 <sarlmnt.soul@gmail.com>
2018-07-15 23:49:08 -0500

    Revert "Revert "sads""
    This reverts commit 25f6add32066fc259d32f8fb2ec811a59f25056f.

--- api/controllers/cliente.js
+++ api/controllers/cliente.js
@@ -664,8 +664,14 @@ function storeUser(req, res) {
     cliente.brandRG = params.brandRG;
     cliente.brandSK = params.brandSK;
     cliente.brandLR = params.brandLR;
-    var email = cliente.email;
+   
     var password = "FormacionesCAE";
+    if (password) {
+        //ecnriptar pasword
+        bcrypt.hash(password, null, null, function (err, hash) {
+            cliente.password = hash;
+        });
+
 
 
     cliente.save((err, clienteStored) => {
@@ -686,7 +692,7 @@ function storeUser(req, res) {
             }
         }
     });
-}
+}}
 
 module.exports = {
     savecliente,
